c2fa0bff38bc24416cddbfab4e2e8fb545361b75
S0n1c <itss0n1cdev@gmail.com>
2021-02-03 20:24:21 -0500

    Include constructor options for creds

--- src/index.ts
+++ src/index.ts
@@ -6,17 +6,18 @@ import { Users } from './stores/Users';
 config();
 
 export class TwitterAPI {
-	client = new Twitter({
-		consumer_key: process.env.TWITTER_CONSUMER_KEY,
-		consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-		access_token_key: process.env.TWITTER_ACCESS_TOKEN,
-		access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-	})
+	client: Twitter
 
 	tweets: Tweets
 	users: Users
 
-	constructor() {
+	constructor(creds?: {consumer_key: string, consumer_secret: string, access_token_key: string, access_token_secret: string}) {
+		this.client = new Twitter({
+			consumer_key: creds.consumer_key || process.env.TWITTER_CONSUMER_KEY,
+			consumer_secret: creds.consumer_secret || process.env.TWITTER_CONSUMER_SECRET,
+			access_token_key: creds.access_token_key || process.env.TWITTER_ACCESS_TOKEN,
+			access_token_secret: creds.access_token_secret || process.env.TWITTER_ACCESS_TOKEN_SECRET
+		});
 		this.tweets = new Tweets(this);
 		this.users = new Users(this);
 	}
